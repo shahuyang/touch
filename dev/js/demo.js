b7683c27609898840785415155b4e3cf1a88d310
shahuyang <shahuyang_c@163.com>
2016-12-07 11:13:01 +0800

    20161207 work over

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -1,86 +1,69 @@
 var touchR ={};
-
+var position=0;
 
 function touch(event){
-  var oDiv = document.getElementById('main');
-  var event = event || window.event;
+    var event = event || window.event;
 
-  switch (event.type){
-    case "touchstart":
-      touchR.startX = event.touches[0].clientX;
-      touchR.startY = event.touches[0].clientY;
-      break;
+    switch (event.type){
+        case "touchstart":
+          touchR.startX = event.touches[0].clientX;
+          touchR.startY = event.touches[0].clientY;
+        break;
 
-    case "touchmove":
-      event.preventDefault();
-      break;
+        case "touchmove":
+        event.preventDefault();
+        break;
 
-    case "touchend":
-      touchR.endX = event.changedTouches[0].clientX;
-      touchR.endY = event.changedTouches[0].clientY;
-      var changeX = (touchR.endX - touchR.startX);
-      var changeY = (touchR.endY - touchR.startY);
-      if(Math.abs(changeX) > Math.abs(changeY)){
-          if(changeX>0){
-              console.log("右滑动"+Math.abs(changeX))
-          }else{
-              console.log("左滑动"+Math.abs(changeX))
-          }
-      }else if(Math.abs(changeX) < Math.abs(changeY)){
-        if(changeX>0){
-            console.log("上滑动"+Math.abs(changeY))
-        }else{
-            console.log("下滑动"+Math.abs(changeY))
-        }
-      }else if(changeX==changeY==0){
-          console.log("click")
-      }
-      break;
-  }
+        //  结果执行
+        case "touchend":
+            touchR.endX = event.changedTouches[0].clientX;
+            touchR.endY = event.changedTouches[0].clientY;
+            var changeX = (touchR.endX - touchR.startX);
+            var changeY = (touchR.endY - touchR.startY);
+            if(Math.abs(changeX) > Math.abs(changeY)){
+                  if(changeX>0){
+                      $('#text').html("右滑动"+Math.abs(changeX));
+                      position = position+100;
+                      if(position>=0){
+                          position=0
+                      };
+                      $("#inner").animate({
+                          left:position+"%"
+                      },400)
+                  }else{
+                      $('#text').html("左滑动"+Math.abs(changeX));
+                      position = position-100;
+                      if(position<=-300){
+                          position=-300
+                      }
+                      $("#inner").animate({
+                          left:position+"%"
+                      },400)
+                  }
+            }else if(Math.abs(changeX) < Math.abs(changeY)){
+                if(changeX>0){
+                    console.log("上滑动"+Math.abs(changeY));
+                }else{
+                    console.log("下滑动"+Math.abs(changeY));
+                }
+            }else if(changeX==changeY==0){
+                console.log("其他")
+            }
+          break;
+    }
 }
 
-function addTouch(){
-
-  document.addEventListener('touchstart',touch,false);
-  document.addEventListener('touchmove',touch,false);
-  document.addEventListener('touchend',touch,false);
+function addTouch(eleId){
+    var oEle = document.getElementById(eleId);
+    oEle.addEventListener('touchstart',touch,false);
+    oEle.addEventListener('touchmove',touch,false);
+    oEle.addEventListener('touchend',touch,false);
 }
 
-addTouch();
 
 
-// function touch(event){
-//   var event = event || window.event;
-//   var oDiv = document.getElementById('main');
-//   console.log(event.type)
-//
-// // 单手指操作，touchstart ， touchmove 只需要监听 event.touches[0] 即可
-//   //            touchend 需要监听  event.changedTouches[0]
-//   switch (event.type){
-//     case "touchstart":
-//       oDiv.innerHTML = "Touch Start（"+event.touches[0].clientX+","+event.touches[0].clientY+"）";
-//       break;
-//     case "touchmove":
-//       event.preventDefault();
-//       oDiv.innerHTML = "Touch Move（"+event.touches[0].clientX+","+event.touches[0].clientY+"）";
-//       break;
-//     case "touchend":
-//       oDiv.innerHTML = "Touch End（"+event.changedTouches[0].clientX+","+event.changedTouches[0].clientY+"）";
-//       break;
-//   }
-// }
-//
-// function load(){
-//   document.addEventListener('touchstart',touch,false);
-//   document.addEventListener('touchmove',touch,false);
-//   document.addEventListener('touchend',touch,false);
-// //测试其他事件
-//   document.addEventListener('mouseover',touch,false);
-//   document.addEventListener('mousemove',touch,false);
-//   document.addEventListener('mousedown',touch,false);
-//   document.addEventListener('mouseup',touch,false);
-//   document.addEventListener('click',touch,false);
-//
-// }
-// //  load();  // 这样写和下面是一样的
-// window.addEventListener('load',load,false);
+addTouch('inner');
+
+
+
+
